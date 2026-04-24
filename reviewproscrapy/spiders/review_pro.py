4fa445285d63185bdd8c7b439c8b32a2372d2954
0xthedance <thedance@amamu.io>
2026-04-24 20:56:03 +0100

    Refactor spider: extract URL helper and split parse into focused methods

--- reviewproscrapy/spiders/review_pro.py
+++ reviewproscrapy/spiders/review_pro.py
@@ -7,25 +7,8 @@ class ReviewProSpider(scrapy.Spider):
     start_urls = ["https://www.shijigroup.com/reviewpro-reputation"]
 
     def parse(self, response):
-        products = []
-        for product in response.css("div.nav-side-pannel__el"):
-            product_sections = []
-            product_title = product.css("p.nav-side-pannel__title::text").get(default="").strip()
-            href = product.xpath("./a/@href").get()
-            product_url = response.urljoin(href) if href else None
-            for section in product.xpath(".//ul[@role='list']/li"):
-                section_title = section.xpath("./a/text()").get(default="").strip()
-                href = section.xpath("./a/@href").get()
-                section_url = response.urljoin(href) if href else None
-                product_sections.append({"title": section_title, "url": section_url})
-            products.append({"title": product_title, "url": product_url, "sections": product_sections})
-
-        solutions = []
-        for solution in response.xpath("//div[contains(@class,'is--solution')]/following-sibling::div//div[@data-w-tab='All']//a[contains(@class,'solutions__card')]"):
-            solution_title = solution.xpath("./div/p/text()").get(default="").strip()
-            href = solution.xpath("./@href").get()
-            solution_url = response.urljoin(href) if href else None
-            solutions.append({"title": solution_title, "url": solution_url})
+        products = self._parse_products(response)
+        solutions = self._parse_solutions(response)
 
         if not solutions and not products:
             self.logger.warning("No data extracted from %s", response.url)
@@ -35,3 +18,37 @@ class ReviewProSpider(scrapy.Spider):
             solutions=solutions,
             products=products,
         )
+
+    def _parse_products(self, response):
+        products = []
+        for product in response.css("div.nav-side-pannel__el"):
+            product_title = product.css("p.nav-side-pannel__title::text").get(default="").strip()
+            sections = [
+                {"title": section.xpath("./text()").get(default="").strip(), "url": self._extract_url(response, section)}
+                for section in product.xpath(".//ul[@role='list']/li/a")
+            ]
+            products.append({"title": product_title, "url": self._extract_url(response, product.xpath("./a")), "sections": sections})
+        return products
+
+    def _parse_solutions(self, response):
+        solutions = [
+            {"title": s.xpath("./div/p/text()").get(default="").strip(), "url": self._extract_url(response, s)}
+            for s in response.xpath("//div[contains(@class,'is--solution')]/following-sibling::div//div[@data-w-tab='All']//a[contains(@class,'solutions__card')]")
+        ]
+        if not solutions:
+            self.logger.warning("Solutions not found in nav, falling back to footer")
+            solutions = self._parse_footer_solutions(response)
+        return solutions
+
+    def _parse_footer_solutions(self, response):
+        footer = response.css("div.footer__top")
+        items = footer.xpath(".//p[normalize-space(text())='Solutions']/../following-sibling::div//li/a")
+        solutions = [
+            {"title": item.xpath("./p/text()").get(default="").strip(), "url": self._extract_url(response, item)}
+            for item in items
+        ]
+        return solutions
+
+    def _extract_url(self, response, node):
+        href = node.xpath("./@href").get()
+        return response.urljoin(href) if href else None
